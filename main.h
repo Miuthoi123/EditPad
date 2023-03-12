183c3b4917b05c3521c67b35dd2585df0f22c1a4
Miuthoi123 <117433468+Miuthoi123@users.noreply.github.com>
2023-03-12 08:38:46 +0700

    Add files via upload

---
+++ main.h
@@ -0,0 +1,16 @@
+#define CM_VIEWHELP 	9082
+#define CM_FONT     	9081
+#define CM_WORDWARP 	9080
+#define CM_PASTE 	    9079
+#define CM_COPY 	    9078
+#define CM_CUT 	        9077
+#define CM_REDO 	    9076
+#define CM_UNDO 	    9075
+#define CM_FILE_NEW 	9074
+#define CM_FILE_SAVE	9073
+#define CM_FILE_SAVEAS	9072
+#define CM_FILE_EXIT	9071
+#define CM_FILE_OPEN	9070
+#define CM_ABOUT        9069
+
+#define IDC_MAIN_TEXT 1001
